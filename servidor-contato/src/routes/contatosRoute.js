f8e8dde68de408dadb66e5bd086aead2d2f056ac
AndradeDay <dayzalvz@gmail.com>
2019-11-05 22:18:08 -0300

    Adicionado a rota post para trazer informações de novos contatos

--- servidor-contato/src/routes/contatosRoute.js
+++ servidor-contato/src/routes/contatosRoute.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const router = express.Router()
+const bodyParser = require("body-parser")
 const controller = require("../controller/contatosController")
 
 router.get("/", controller.getAll)
-router.get("/:id", controller.getById)
+router.post("/criar", bodyParser.json(), controller.add) // o bodyPasrer. json informa que está 
+// recebendo uma importação de dados em json 
 
 module.exports = router
